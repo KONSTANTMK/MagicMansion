ce53c0b9ed26212bca55b0a9ec85bc61d6f58eef
konstantmk <konstantmk@gmail.com>
2023-01-08 16:11:13 +0600

    Changes for connection to DB

---
+++ MagicMansion_MansionAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicMansion_MansionAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicMansion_MansionAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Mansion> Mansions { get; set; }
+    }
+}
--- MagicMansion_MansionAPI/Models/Mansion.cs
+++ MagicMansion_MansionAPI/Models/Mansion.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MagicMansion_MansionAPI.Models
 {
     public class Mansion
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Details { get; set; }
