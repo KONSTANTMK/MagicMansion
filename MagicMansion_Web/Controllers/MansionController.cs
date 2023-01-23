6a8ac6dc1c15f51ad061f1b90ddfc7177e3c3512
konstantmk <konstantmk@gmail.com>
2023-01-23 19:06:14 +0600

    Add Mansion View

---
+++ MagicMansion_Web/Controllers/MansionController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MagicMansion_Web.Models;
+using MagicMansion_Web.Models.Dto;
+using MagicMansion_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicMansion_Web.Controllers
+{
+	public class MansionController : Controller
+	{
+		private readonly IMansionService _mansionService;
+		public readonly IMapper _mapper;
+		public MansionController(IMansionService mansionService,IMapper mapper)
+		{
+			_mansionService= mansionService;
+			_mapper= mapper;
+		}
+		public async Task<IActionResult> IndexMansion()
+		{
+			List<MansionDTO> list = new();
+
+
+			var response = await _mansionService.GetAllAsync<APIResponse>();
+			if(response!=null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<MansionDTO>>(Convert.ToString(response.Result));
+			}
+
+			return View();
+		}
+	}
+}
