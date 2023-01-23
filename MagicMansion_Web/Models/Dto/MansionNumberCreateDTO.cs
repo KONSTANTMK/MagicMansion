bd8a587ec945213e70c93c873a876ca27e9bf6dc
konstantmk <konstantmk@gmail.com>
2023-01-23 15:26:24 +0600

    Updates of created project

---
+++ MagicMansion_Web/Models/Dto/MansionNumberCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicMansion_Web.Models.Dto
+{
+    public class MansionNumberCreateDTO
+    {
+        [Required]
+        public int MansionNo { get; set; }
+        [Required]
+        public int MansionID { get; set; }
+        public string SpecialDetails { get; set; }
+    
+    }
+}
---
+++ MagicMansion_Web/Models/Dto/MansionNumberUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicMansion_Web.Models.Dto
+{
+    public class MansionNumberUpdateDTO
+    {
+        [Required]
+        public int MansionNo { get; set; }
+		[Required]
+		public int MansionID { get; set; }
+		public string SpecialDetails { get; set; }
+    
+    }
+}
