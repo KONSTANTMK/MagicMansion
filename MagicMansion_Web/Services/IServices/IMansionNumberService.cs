2126d7575dad405183a3209b7754870019564bc1
konstantmk <konstantmk@gmail.com>
2023-01-24 17:16:08 +0600

    Create mansion number service

---
+++ MagicMansion_Web/Services/IServices/IMansionNumberService.cs
@@ -0,0 +1,13 @@
+﻿using MagicMansion_Web.Models.Dto;
+
+namespace MagicMansion_Web.Services.IServices
+{
+	public interface IMansionNumberService
+	{
+		Task<T> GetAllAsync<T>();
+		Task<T> GetAsync<T>(int id);
+		Task<T> CreateAsync<T>(MansionNumberCreateDTO dto);
+		Task<T> UpdateAsync<T>(MansionNumberUpdateDTO dto);
+		Task<T> DeleteAsync<T>(int id);
+	}
+}
---
+++ MagicMansion_Web/Services/MansionNumberService.cs
@@ -0,0 +1,64 @@
+﻿using MagicMansion_Utility;
+using MagicMansion_Web.Models;
+using MagicMansion_Web.Models.Dto;
+using MagicMansion_Web.Services.IServices;
+
+namespace MagicMansion_Web.Services
+{
+	public class MansionNumberService : BaseService, IMansionNumberService
+	{
+		private readonly IHttpClientFactory _clientFactory;
+		private string mansionUrl;
+		public MansionNumberService(IHttpClientFactory clientFactory,IConfiguration configuration):base(clientFactory)
+		{
+			_clientFactory= clientFactory;
+			mansionUrl = configuration.GetValue<string>("ServiceUrls:MansionApi");
+		}
+		public Task<T> CreateAsync<T>(MansionNumberCreateDTO dto)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				ApiType = SD.ApiType.POST,
+				Data = dto,
+				Url = mansionUrl + "/api/MansionNumberAPI"
+            });
+		}
+
+		public Task<T> DeleteAsync<T>(int id)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				ApiType = SD.ApiType.DELETE,
+				Url = mansionUrl + "/api/MansionNumberAPI/" + id
+			});
+		}
+
+		public Task<T> GetAllAsync<T>()
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				ApiType = SD.ApiType.GET,
+				Url = mansionUrl + "/api/MansionNumberAPI"
+            });
+		}
+
+		public Task<T> GetAsync<T>(int id)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				ApiType = SD.ApiType.GET,
+				Url = mansionUrl + "/api/MansionNumberAPI/" + id
+			});
+		}
+
+		public Task<T> UpdateAsync<T>(MansionNumberUpdateDTO dto)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				ApiType = SD.ApiType.PUT,
+				Data = dto,
+				Url = mansionUrl + "/api/MansionNumberAPI/" + dto.MansionNo
+			});
+		}
+	}
+}
