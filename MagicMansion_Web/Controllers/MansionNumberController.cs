acb89a8ba9f8a7e26362d7e9934153a4847857d9
konstantmk <konstantmk@gmail.com>
2023-01-27 22:30:17 +0600

    Create Villa Number GET

--- MagicMansion_Web/Controllers/MansionNumberController.cs
+++ MagicMansion_Web/Controllers/MansionNumberController.cs
@@ -1,20 +1,25 @@
 ﻿using AutoMapper;
 using MagicMansion_Web.Models;
 using MagicMansion_Web.Models.Dto;
+using MagicMansion_Web.Models.VM;
 using MagicMansion_Web.Services;
 using MagicMansion_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace MagicMansion_Web.Controllers
 {
     public class MansionNumberController : Controller
     {
         private readonly IMansionNumberService _mansionNumberService;
-        public readonly IMapper _mapper;
-        public MansionNumberController(IMansionNumberService mansionNumberService, IMapper mapper)
+		private readonly IMansionService _mansionService;
+		public readonly IMapper _mapper;
+        public MansionNumberController(IMansionNumberService mansionNumberService,IMansionService mansionService,IMapper mapper)
         {
             _mansionNumberService = mansionNumberService;
+			_mansionService = mansionService;
             _mapper = mapper;
         }
         public async Task<IActionResult> IndexMansionNumber()
@@ -30,5 +35,95 @@ namespace MagicMansion_Web.Controllers
 
             return View(list);
         }
-    }
+		public async Task<IActionResult> CreateMansionNumber()
+		{
+			MansionNumberCreateVM mansionNumberVM = new();
+			var response = await _mansionService.GetAllAsync<APIResponse>();
+			if (response != null && response.IsSuccess)
+			{
+				mansionNumberVM.MansionList = JsonConvert.DeserializeObject<List<MansionDTO>>
+					(Convert.ToString(response.Result)).Select(i =>new SelectListItem
+					{
+						Text = i.Name,
+						Value = i.Id.ToString(),
+					});
+			}
+			return View(mansionNumberVM);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> CreateMansionNumber(MansionNumberCreateDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _mansionNumberService.CreateAsync<APIResponse>(model);
+				if (response != null && response.IsSuccess)
+				{
+					return RedirectToAction(nameof(IndexMansionNumber));
+				}
+			}
+
+			return View(model);
+		}
+
+		//public async Task<IActionResult> UpdateMansionNumber(int mansionId)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		var response = await _mansionNumberService.GetAsync<APIResponse>(mansionId);
+		//		if (response != null && response.IsSuccess)
+		//		{
+		//			MansionNumberDTO model = JsonConvert.DeserializeObject<MansionNumberDTO>(Convert.ToString(response.Result));
+		//			return View(_mapper.Map<MansionNumberUpdateDTO>(model));
+		//		}
+		//		return NotFound();
+
+		//	}
+		//	return View();
+		//}
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public async Task<IActionResult> UpdateMansionNumber(MansionNumberUpdateDTO model)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		var response = await _mansionNumberService.UpdateAsync<APIResponse>(model);
+		//		if (response != null && response.IsSuccess)
+		//		{
+		//			return RedirectToAction(nameof(IndexMansionNumber));
+		//		}
+		//	}
+
+		//	return View(model);
+		//}
+		//public async Task<IActionResult> DeleteMansionNumber(int mansionId)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		var response = await _mansionNumberService.GetAsync<APIResponse>(mansionId);
+		//		if (response != null && response.IsSuccess)
+		//		{
+		//			MansionNumberDTO model = JsonConvert.DeserializeObject<MansionNumberDTO>(Convert.ToString(response.Result));
+		//			return View(model);
+		//		}
+		//		return NotFound();
+
+		//	}
+		//	return View();
+		//}
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public async Task<IActionResult> DeleteMansionNumber(MansionNumberDTO model)
+		//{
+		//	var response = await _mansionNumberService.DeleteAsync<APIResponse>(model.Id);
+		//	if (response != null && response.IsSuccess)
+		//	{
+		//		return RedirectToAction(nameof(IndexMansionNumber));
+		//	}
+
+
+
+		//	return View(model);
+		//}
+	}
 }
---
+++ MagicMansion_Web/Models/VM/MansionNumberCreateVM.cs
@@ -0,0 +1,17 @@
+﻿using MagicMansion_MansionAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MagicMansion_Web.Models.VM
+{
+	public class MansionNumberCreateVM
+	{
+		public MansionNumberCreateVM()
+		{
+			MansionNumber = new MansionCreateDTO();
+		}
+		public MansionCreateDTO MansionNumber { get; set; }
+		[ValidateNever]
+		public IEnumerable<SelectListItem> MansionList { get; set; }
+	}
+}
