0ef26452d7d874b0267976ce0ba7c27000e8951b
konstantmk <konstantmk@gmail.com>
2023-01-28 18:55:39 +0600

    Add User Repository

---
+++ MagicMansion_MansionAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicMansion_MansionAPI.Models.Dto;
+using MagicMansion_MansionAPI.Models;
+
+namespace MagicMansion_MansionAPI.Repository.IRepostiory
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ MagicMansion_MansionAPI/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using MagicMansion_MansionAPI.Data;
+using MagicMansion_MansionAPI.Models.Dto;
+using MagicMansion_MansionAPI.Models;
+using MagicMansion_MansionAPI.Repository.IRepostiory;
+
+namespace MagicMansion_MansionAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
